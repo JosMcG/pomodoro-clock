89c39509d61b7e3ca55bb6bfc6b36571a534e7d1
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-01 21:40:27 -0400

    eliminated sound state variable by utilizing audio element; works in all browsers; passes all freeCodeCamp test cases

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -9,24 +9,23 @@ import './Timer.css';
 const Timer = (props) => {
     const {timer, setTimer} = props;
     const {showTime, setShowTime} = props;
-    const [sound, setSound] = useState();
 
     function startStop() {
-        setTimer(toggleRunTimer(timer));
-        setSound(new Audio(chime));
-        
+        setTimer(toggleRunTimer(timer));  
     }
 
     function reset () {
         const newTimer = resetTimer(timer);
         setTimer(newTimer);
         setShowTime(displayTime(newTimer));
+        document.getElementById('beep').pause();
+        document.getElementById('beep').currentTime = 0;
     }
    
     useEffect (() => {
         const interval = setInterval (() => {
             if(timer.runTimer) { 
-                setTimer(decrementTime(timer, sound));
+                setTimer(decrementTime(timer, document.getElementById('beep')));
                 setShowTime(displayTime(timer));
             }
         }, 1000);
@@ -41,7 +40,7 @@ const Timer = (props) => {
                 {props.timer.working ? <h2 className="session-label">Work Session</h2> : <h2 className="session-label">Break Time</h2>}
             </div>
             <p id="time-left">{showTime}</p>
-            <audio id="beep" src={sound}></audio>
+            <audio id="beep" src={chime} preload="auto"></audio>
             <button onClick={() => startStop()} className="buttons" id="start_stop"><img id="play-pause-image" src={playPause} alt="Start/Pause"/></button>
             <button onClick={() => reset()} className="buttons" id="reset"><img id="reset-image" src={refresh} alt="Reset"/></button>
         </div>
