f85a5dc6e6aa0dfcfca0084368722259899a6117
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 12:03:57 -0400

    firebase initialization

---
+++ src/services.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJKO9WKcm7AUTXqijpTBaYgjM8rD94UGs",
+  authDomain: "pomodoro-clock-c003b.firebaseapp.com",
+  projectId: "pomodoro-clock-c003b",
+  storageBucket: "pomodoro-clock-c003b.appspot.com",
+  messagingSenderId: "28176037894",
+  appId: "1:28176037894:web:1e2cf6d0148b68cfc12b87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
